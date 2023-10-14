80ad4ea24ff4d4a86cf300310fa9587a4af36a0d
Adrian Breiding <ad123br@gmail.com>
2023-10-14 19:17:43 +0200

    Zoom Card Fix
    After adding Networking the zoom card function stopped working due to instantiation of doubled network identity: fix was to create a new prefab and instantiate that and copy essential data.

--- Assets/Scripts/CardZoom.cs
+++ Assets/Scripts/CardZoom.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardZoom : MonoBehaviour
 {
     public GameObject Canvas;
     private GameObject zoomCard;
+    public GameObject zoom;
 
     public void Awake()
     {
@@ -14,11 +16,15 @@ public class CardZoom : MonoBehaviour
 
     public void OnHoverEnter()
     {
-        zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y+ 250), Quaternion.identity);
-        zoomCard.transform.SetParent(Canvas.transform, false);
+        zoomCard = Instantiate(zoom, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 250), Quaternion.identity);
+        zoomCard.transform.SetParent(Canvas.transform, true);
         zoomCard.layer = LayerMask.NameToLayer("Zoom");
+        
         RectTransform rect = zoomCard.GetComponent<RectTransform>();
         rect.sizeDelta = new Vector2(240, 344);
+        Image imageSrc = gameObject.GetComponent<Image>();
+        Image imageTarget = zoomCard.GetComponent<Image>();
+        imageTarget.color = imageSrc.color;
     }
 
     public void onHoverExit()
